a16d09bcc42d9a6b9a771260e8a03995160c5e29
Cleiton da Silva Mendonca <cleiton.mendonca@gmail.com>
2020-09-10 10:42:29 -0400

    Added a Mapping Profile Class where we're mapping your domain entities with view model classes

---
+++ src/Application/Invoices/MappingProfiles/InvoiceMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using Application.Invoices.Commands;
+using Application.Invoices.ViewModels;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Invoices.MappingProfiles
+{
+    public class InvoiceMappingProfile : Profile
+    {
+        public InvoiceMappingProfile()
+        {
+            CreateMap<Invoice, InvoiceViewModel>();
+            CreateMap<InvoiceItem, InvoiceItemViewModel>();
+
+            CreateMap<InvoiceViewModel, Invoice>();
+            CreateMap<InvoiceItemViewModel, InvoiceItem>();
+
+            CreateMap<CreateInvoiceCommand, Invoice>();
+        }
+    }
+}
